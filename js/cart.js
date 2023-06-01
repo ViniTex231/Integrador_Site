5a5452dad79b95ddfb9df696283f81bcb298f42d
ViniTex231 <viniteixeira2828@gmail.com>
2023-06-01 10:42:42 -0300

    alteração nos comentarios

--- js/cart.js
+++ js/cart.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Obtém todos os elementos necessários do carrinho
+    // Get all elements
     var cartItems = document.querySelectorAll(".cart-content tr");
     var subtotalElement = document.querySelector(".total-price");
     var totalElement = document.querySelector(".total-price strong");
     var confirmButton = document.querySelector(".normal");
 
-    // Atualiza os totais de preço e subtotal
+    // Update Subtotal and Total prices
     function updateTotals() {
         var subtotal = 0;
         var total = 0;
@@ -29,21 +29,21 @@ document.addEventListener("DOMContentLoaded", function() {
         totalElement.textContent = "R$ " + total.toFixed(2);
     }
 
-    // Adiciona um event listener para atualizar os totais sempre que a quantidade do item mudar
+    // Crate a event listener for update the totals when the itens quantity updates 
     cartItems.forEach(function(item) {
         var quantityElement = item.querySelector(".cart-quantity input");
         quantityElement.addEventListener("change", updateTotals);
     });
 
-    // Remove o item do carrinho ao clicar no botão de remoção
+    // Remove a item for cart
     cartItems.forEach(function(item) {
         var removeButton = item.querySelector(".cart-remove");
         removeButton.addEventListener("click", function() {
-            item.remove();
-            updateTotals();
+            item.remove()
+            updateTotals()
         });
     });
 
-    // Chama a função de atualização dos totais no carregamento inicial
+    // Call the update function in the start
     updateTotals();
 });
